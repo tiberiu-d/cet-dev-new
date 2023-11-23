643e83ad44fbbf1835c7f11245c0d1d8fa486033
Tiberiu Dubău <uix08410@vitesco.com>
2023-11-23 15:06:46 +0200

    dummy data

---
+++ database/dummy_PreviewEscalations.js
@@ -0,0 +1,43 @@
+export const dbPreviewEscalations = [
+  {
+    id: 1,
+    title: "Donec quis orci eget orci vehicula condimentum.",
+    type: "Site Furnishings",
+    escal_date: "10.12.2022",
+    descal_date: "26.08.2023",
+    est_descal_date: "30.07.2023",
+    customer_group: "CustGroup004",
+    level: {
+      name: "Level One",
+      color: "Yellow",
+    },
+  },
+  {
+    id: 2,
+    title:
+      "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.",
+    type: "Curb & Gutter",
+    escal_date: "25.08.2023",
+    descal_date: "22.09.2023",
+    est_descal_date: "10.05.2023",
+    customer_group: "CustGroup004",
+    level: {
+      name: "Level One",
+      color: "Red",
+    },
+  },
+  {
+    id: 3,
+    title:
+      "Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.",
+    type: "Framing (Wood)",
+    escal_date: "11.01.2023",
+    descal_date: "09.01.2023",
+    est_descal_date: "04.01.2023",
+    customer_group: "CustGroup004",
+    level: {
+      name: "Level Three",
+      color: "Red",
+    },
+  },
+];

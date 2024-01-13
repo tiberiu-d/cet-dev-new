6f9e8789792c68173dfe294cfac602b067d61236
Tiberiu Dubău <tibi.dubau@icloud.com>
2024-01-13 16:10:08 +0200

    testing new functionalities

---
+++ app/test/dummy-text.tsx
@@ -0,0 +1,132 @@
+const DummyTextLG = () => {
+  return (
+    <div className="text-justify">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur
+      eligendi veritatis modi cum voluptas qui sit corrupti, dolor dignissimos,
+      velit cumque omnis aut et quisquam quaerat! Esse et eligendi mollitia,
+      temporibus eum sapiente perspiciatis soluta. Blanditiis omnis asperiores,
+      eius doloribus illum et modi in tempore velit deleniti expedita. Rem nam
+      amet sapiente at in officiis illo quisquam repellendus quo alias quibusdam
+      commodi pariatur sed qui, obcaecati consectetur molestias harum debitis
+      deserunt. Quas sed deleniti ex inventore excepturi distinctio,
+      reprehenderit facere dolore placeat nulla molestiae aliquam provident
+      quidem tempora accusantium numquam aperiam quia nobis consectetur. Error
+      mollitia vel tempore iste quia necessitatibus molestiae odit voluptate
+      aliquam, doloribus nam? Vero praesentium, culpa, fugiat nisi delectus,
+      molestias quidem quo excepturi architecto facere tenetur. Consequuntur
+      maxime veritatis autem dolorum dolores quidem assumenda error nam quod id
+      et quaerat, est beatae, cumque tempora asperiores fugit reprehenderit
+      deserunt vitae facere hic enim tenetur sit! Officiis debitis consequuntur,
+      atque deleniti eligendi ex consequatur provident soluta perferendis
+      consectetur iure vel, tenetur aliquam exercitationem quaerat laborum
+      maiores incidunt! Sunt cumque possimus labore laborum qui. Tenetur
+      consectetur saepe at eligendi facere sint neque dolor doloribus quisquam
+      odit officiis eos, iusto aperiam accusamus, distinctio beatae laudantium
+      magni cupiditate corrupti dolorem, optio officia. Exercitationem
+      laudantium error nemo dolore repudiandae facilis nam in pariatur mollitia
+      voluptas harum at similique quis quisquam dolorem voluptates aliquam ab
+      sunt, unde, omnis minus provident non distinctio? Officiis consequatur
+      molestiae dolore quae, accusantium blanditiis, mollitia similique, natus
+      distinctio laborum beatae. Provident, minima labore dicta officia aliquid
+      doloremque quidem, molestias rerum eius voluptatibus magnam! Non officiis,
+      unde sequi et autem aut nihil amet! Distinctio, corporis doloremque
+      molestiae omnis aliquam dolorem neque, nostrum quas harum cupiditate
+      assumenda laboriosam! Explicabo quibusdam cum pariatur nemo, expedita
+      praesentium sit inventore quod omnis adipisci est delectus non nostrum
+      modi harum voluptas nesciunt? Amet, ea qui, deleniti eligendi assumenda
+      veniam accusamus consequatur illo expedita rerum, commodi autem molestias
+      nobis animi iure doloribus asperiores odio odit dolor minus? Id sunt vel
+      labore velit unde cumque, laboriosam quaerat voluptate reiciendis
+      praesentium animi reprehenderit dolore quasi magni ullam expedita nemo
+      asperiores commodi laborum? Iure perferendis excepturi, natus hic, magni
+      vero reiciendis voluptatibus vel dolore, nobis tenetur laboriosam delectus
+      non optio! Quae, repudiandae doloribus. Qui praesentium, ut doloribus eius
+      delectus nisi ratione sint nobis nemo nihil obcaecati itaque, cupiditate
+      quam est! Libero asperiores magnam soluta consectetur! In quam, doloribus
+      ad corrupti blanditiis rem consequatur alias, maxime at id nostrum
+      voluptatem mollitia magnam perferendis facere. Ratione aliquam, ipsa
+      repudiandae voluptatem provident cum, consequuntur nisi nihil odit ut eum
+      ullam impedit natus delectus. Ipsum ratione commodi enim velit officiis
+      voluptates nam tempora dolores minima a? Mollitia animi nam repellat optio
+      consequatur eius quisquam totam, sequi alias excepturi similique quod
+      minus sed distinctio sunt facilis saepe, et molestias! Voluptatem
+      accusantium ex incidunt deleniti beatae quasi, odio atque officiis, sed
+      aspernatur amet aut cupiditate dolorem commodi at nam rem iure asperiores
+      nobis doloremque esse eum veniam autem quas? Aspernatur ex perferendis
+      molestiae at, fuga, eveniet facilis aperiam natus nulla necessitatibus
+      dolor. Cum error fuga ut, vero sequi accusamus! Quaerat nesciunt modi,
+      unde inventore maiores reprehenderit et nemo animi, odit praesentium
+      maxime optio, laudantium facere nostrum culpa? Repellendus blanditiis
+      eaque recusandae autem illo! Ratione accusamus harum porro tempore, magni
+      blanditiis dolor at repudiandae asperiores debitis suscipit odit dolore
+      consequatur, dolorem reprehenderit eveniet quae ullam? Perferendis alias
+      vel similique? Est non ipsum ea vitae, pariatur expedita et in excepturi
+      ut nam quidem accusantium culpa eius voluptates iure soluta quas, corrupti
+      porro repellat commodi deleniti ex! Sunt eius quidem facere eveniet vel
+      unde consequatur ipsum rerum, asperiores autem labore ad suscipit tempore
+      eos harum inventore molestias magni cumque? Aliquid qui iste officia natus
+      vel quasi? Accusantium exercitationem neque ratione iusto omnis cumque
+      explicabo. Odit soluta iure amet minus dignissimos labore qui, unde
+      perspiciatis mollitia voluptatem impedit ea nemo ab beatae laudantium
+      tempora dolorem ducimus expedita vitae consectetur molestias nostrum animi
+      recusandae quae? Maiores, ad omnis? Quasi in corrupti hic magnam molestiae
+      a ipsa omnis fugiat, nulla error, maxime temporibus, optio qui assumenda
+      atque voluptatum? Asperiores at voluptatum, iusto quasi ratione dolor
+      quibusdam quia aliquid, aperiam sint ullam voluptatem, quos dolorem
+      dolore. Fuga voluptatem harum deleniti exercitationem dolore similique
+      laboriosam quidem iure consequatur quisquam beatae deserunt assumenda
+      vitae unde eius quas accusamus illum pariatur qui, debitis ad. Odit quos
+      sit nulla incidunt suscipit, ullam fuga ducimus! Non harum animi enim,
+      quidem perferendis autem cum dolorum vero velit mollitia consequatur.
+      Corporis, earum nostrum, voluptas enim labore ea repellendus asperiores
+      sequi delectus atque voluptate, officia officiis a recusandae
+      reprehenderit ipsam doloremque autem possimus esse rerum! Ipsa earum
+      beatae ipsam iste debitis. Eveniet et, nisi autem expedita quod pariatur
+      sint repellendus blanditiis, corrupti earum, ut illum asperiores vitae
+      repellat quasi praesentium cumque numquam voluptate totam delectus?
+      Aliquid id voluptates adipisci quo, dolorem est magni quos sint rerum
+      accusantium facilis minima dolore pariatur sapiente praesentium vero
+      quisquam fuga provident consequatur porro animi! Fuga nobis voluptas,
+      veritatis, similique neque molestiae consectetur unde error saepe sint
+      culpa laudantium illum, illo delectus dolores maiores quia enim deserunt
+      vitae deleniti eum. Voluptate odio, ab est eveniet quisquam incidunt
+      nostrum officia! Culpa vero nihil eum in eligendi provident vitae soluta
+      similique repellendus quidem deserunt dolor, hic odit ea atque nostrum
+      dolores. Nostrum accusamus laborum dicta similique tenetur inventore eum
+      nihil beatae ratione excepturi. Numquam, corrupti? Nostrum explicabo fuga
+      nesciunt blanditiis molestias, ut aperiam commodi, ipsam minus veniam
+      architecto asperiores dolor illum. Ipsam officia voluptates, ratione
+      deleniti beatae doloribus repellat obcaecati expedita, quaerat
+      repellendus, reprehenderit velit qui iste? Et nesciunt voluptatibus
+      voluptas obcaecati dicta, quasi doloribus assumenda consequatur illo
+      recusandae eius sequi. Consequatur totam perspiciatis voluptate ducimus
+      molestias. Expedita incidunt iste sunt culpa tempora repudiandae quaerat,
+      accusantium id delectus autem ratione nisi veniam modi officia, quae
+      facere qui! Ea iusto sit obcaecati voluptatum eius quas exercitationem eum
+      accusamus velit laborum culpa perspiciatis, fuga dolores iure totam rerum
+      magni repudiandae deserunt harum neque officiis voluptas quidem
+      laudantium. Dignissimos deleniti quisquam, sunt deserunt incidunt
+      explicabo repudiandae repellendus omnis quis dolor eaque illo consequatur
+      exercitationem doloremque accusamus a itaque molestiae!
+    </div>
+  );
+};
+const DummyTextMD = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quis cumque
+      tempora harum commodi beatae voluptatibus vero fugit quos molestiae quidem
+      ut ad repudiandae magnam, similique omnis libero sunt placeat aspernatur
+      laborum odio qui sequi perspiciatis. Voluptatibus sapiente ullam laborum
+      harum, omnis veritatis totam placeat quidem temporibus cumque. Illum
+      debitis eveniet architecto, deserunt ratione et quas esse nesciunt
+      assumenda perspiciatis quam provident quos magni quidem similique
+      cupiditate ipsa, obcaecati, minima velit totam accusamus? Neque laudantium
+      molestias, officia quas repudiandae quaerat cum unde numquam eius tempore
+      ad aut quia exercitationem animi fuga accusamus suscipit voluptatibus.
+      Dicta esse possimus illo quibusdam corrupti.
+    </div>
+  );
+};
+
+export { DummyTextLG, DummyTextMD };
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,161 +1,50 @@
+"use client";
+import { DummyTextLG, DummyTextMD } from "./dummy-text";
+
+import { useRef, useState } from "react";
+
 const TestPage = () => {
-  return <div>TestPage</div>;
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+  const [isScrolledToBottom, setIsScrolledToBottom] = useState(false);
+
+  const fctScrollToBottom = () => {
+    console.log(
+      `viewport width: ${window.innerWidth} height: ${window.innerHeight}`
+    );
+
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer) {
+      const scrollHeight = scrollContainer.scrollHeight;
+      const elementHeight = scrollContainer.clientHeight;
+      const scrollTo = scrollHeight - elementHeight;
+
+      scrollContainer.scrollTo({
+        top: scrollTo,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <div className="w-full bg-gray-500 h-full flex flex-col items-top">
+      <div className="w-full bg-pink-200">
+        <button onClick={fctScrollToBottom} disabled={isScrolledToBottom}>
+          click me
+        </button>
+      </div>
+      <div className="w-full bg-blue-200 pb-6">
+        <DummyTextMD />
+      </div>
+      <div
+        className="w-full bg-green-200 flex-grow overflow-y-auto relative"
+        ref={scrollContainerRef}
+        onScroll={() => setIsScrolledToBottom(false)}
+      >
+        <div className=" bg-red-400 sticky top-0 p-4">test</div>
+        <DummyTextLG />
+        <DummyTextLG />
+      </div>
+    </div>
+  );
 };
 export default TestPage;
-// {/* row 1 */}
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>QKAM</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.QKAM}</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Basic Data</CardTitle>
-//   </CardHeader>
-//   <CardContent className="flex flex-col">
-//     <div className="flex items-center gap-2">
-//       <p className="text-sm min-w-[150px]">modified on</p>
-//       <Separator orientation="vertical" className="mx-2 h-6" />
-//       <p>{}</p>
-//     </div>
-//     <div className="flex items-center gap-2">
-//       <p className="text-sm min-w-[150px]">status</p>
-//       <Separator orientation="vertical" className="mx-2 h-6" />
-//       <p>{onePager?.STATUS}</p>
-//     </div>
-//   </CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Sub Type</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.TYPE}</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Additional Dates</CardTitle>
-//   </CardHeader>
-//   <CardContent className="flex flex-col">
-//     <div className="flex items-center gap-2">
-//       <p className="text-sm min-w-[150px]">de-escalation date</p>
-//       <Separator orientation="vertical" className="mx-2 h-6" />
-//       <p>{onePager?.DESCAL_DATE}</p>
-//     </div>
-//     <div className="flex items-center gap-2">
-//       <p className="text-sm min-w-[150px]">est. de-escalation date</p>
-//       <Separator orientation="vertical" className="mx-2 h-6" />
-//       <p>{}</p>
-//     </div>
-//   </CardContent>
-// </Card>
-// {/* row 2 */}
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Customer Group</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.CUSTOMER_GROUP}</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Customer Brand</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.CUSTOMER_BRAND}</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Customer Location</CardTitle>
-//   </CardHeader>
-//   <CardContent>no data</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Customer Escalation Level</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.LEVEL}</CardContent>
-// </Card>
-// {/* row 3 */}
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Additional Customer Brands</CardTitle>
-//   </CardHeader>
-//   <CardContent>no data</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Vitesco Division</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.VT_DIVISION_NAME}</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Vitesco Business Unit</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.VT_BUSINESS_UNIT_NAME}</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Vitesco Plant</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.VT_PLANT}</CardContent>
-// </Card>
-// {/* row 4 */}
-// <Card className="col-span-2 hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Customer Impact</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.CUSTOMER_IMPACT}</CardContent>
-// </Card>
-// <Card className="col-span-2 hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Consumer Impact</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.CONSUMER_IMPACT}</CardContent>
-// </Card>
-// {/* row 5 */}
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Affected parts & vehicles</CardTitle>
-//   </CardHeader>
-//   <CardContent className="flex flex-col">
-//     <div className="flex items-center gap-2">
-//       <p className="text-sm min-w-[150px]">affected parts</p>
-//       <Separator orientation="vertical" className="mx-2 h-6" />
-//       <p>{onePager?.AFFECTED_PARTS}</p>
-//     </div>
-//     <div className="flex items-center gap-2">
-//       <p className="text-sm min-w-[150px]">affected vehicles</p>
-//       <Separator orientation="vertical" className="mx-2 h-6" />
-//       <p>{onePager?.AFFECTED_VEHICLES}</p>
-//     </div>
-//   </CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Vehicle details</CardTitle>
-//   </CardHeader>
-//   <CardContent className="flex flex-col">
-//     <div className="flex items-center gap-2">
-//       <p className="text-sm min-w-[150px]">platform</p>
-//       <Separator orientation="vertical" className="mx-2 h-6" />
-//       <p>{onePager?.CAR_PLATFORMS}</p>
-//     </div>
-//     <div className="flex items-center gap-2">
-//       <p className="text-sm min-w-[150px]">model</p>
-//       <Separator orientation="vertical" className="mx-2 h-6" />
-//       <p>{onePager?.CAR_MODELS}</p>
-//     </div>
-//   </CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Supplier</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.SUPPLIER}</CardContent>
-// </Card>
-// <Card className="hover:bg-gray-50">
-//   <CardHeader>
-//     <CardTitle>Is Reccuring?</CardTitle>
-//   </CardHeader>
-//   <CardContent>{onePager?.RECURRING}</CardContent>
-// </Card>

6528019b5b8a6041b7f65e0339f1fdc36bb6a0f9
Tiberiu Dubău <uix08410@vitesco.com>
2024-01-15 14:57:19 +0200

    moved types around

--- types/escalation.ts
+++ types/escalation.ts
@@ -10,3 +10,36 @@ export type EscalationType = {
 };
 
 export type PartialEscalationType = Partial<EscalationType>;
+
+export type OnePagerType = {
+  ID: string;
+  QKAM: string;
+  CREATED_ON: string;
+  STATUS: string;
+  TYPE: string;
+  ESCAL_DATE: string;
+  DESCAL_DATE: string;
+  CUSTOMER_GROUP_CODE: string;
+  CUSTOMER_GROUP: string;
+  CUSTOMER_BRAND_CODE: string;
+  CUSTOMER_BRAND: string;
+  CUSTOMER_LOCATIONS: string;
+  LEVEL: string;
+  ADDITIONAL_BRANDS: string;
+  VT_DIVISION_CODE: string;
+  VT_DIVISION_NAME: string;
+  VT_BUSINESS_UNIT_CODE: string;
+  VT_BUSINESS_UNIT_NAME: string;
+  VT_PLANT_CODE: string;
+  VT_PLANT: string;
+  TITLE: string;
+  AFFECTED_PARTS: number;
+  AFFECTED_VEHICLES: number;
+  DESCRIPTION: string;
+  CUSTOMER_IMPACT: string;
+  RECURRING: boolean;
+  SUPPLIER: string;
+  CONSUMER_IMPACT: string;
+  CAR_MODELS: string;
+  CAR_PLATFORMS: string;
+};
